149a02e7547e62ea43592ebeb7a993cc54a184d3
xchanin <shannonwbruns@gmail.com>
2021-08-19 13:17:19 -0600

    comment out code that appears to not be used

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import { PublicWebApplication } from './app/PublicWebApplication.js'
 import { FlowTool } from './controls/FlowTool.js';
 
-// import { Variables } from './utils/variables.js';
-
-// import { Events } from './utils/events.js';
-// import FlowToolOriginal02 from './controls/FlowToolOriginal02.js';
-// import { DataFlowBaseClass } from './base-classes/data-flow-base-class.js';
-// import { BaseFunctions } from './base-classes/base-functions.js';
-
-
 export { PublicWebApplication, FlowTool }
